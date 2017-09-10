6b8b4d162b152fb2b4518e087c3bbf49d445f145
Martin Virtel <martin.virtel@gmail.com>
2017-09-10 23:24:19 +0200

    base64 filter etc

--- xattr-edit.py
+++ xattr-edit.py
@@ -3,21 +3,22 @@ import os
 from pathlib import Path
 from xattr import xattr
 import fire
-import attrdict
-from jinja2 import Environment, PackageLoader, select_autoescape
+import archieml
+from jinja2 import Environment
 from collections import namedtuple
 import stat
-
+import base64
 
 env = Environment()
 
+env.filters["base64"]=lambda a : base64.b64encode(str(a).encode("utf-8")).decode("utf-8")
 
 template=env.from_string("""
 {% for file in items %}
+{{ "{files." }}{{ file.stat.ino }}{{ "}" }}
 path: {{file.path}}
+--------------------------------------------------------------------------------------
 name: {{file.path.name}}
-dir: {{file.path.parent}}
-inode: {{file.stat.ino}}
 
 {%- for k,v in file.attr.items() : %}
 {{ k }}: {{ v }}
@@ -32,13 +33,25 @@ inode: {{file.stat.ino}}
 class attrwrapper(xattr) :
 
     def get(self,item) :
+        if item.find("user.")==-1 :
+            item=f"user.{item}"
         e=xattr.get(self,item)
-        print(f"get of {item}")
         if type(e) == type(b'') :
             e=e.decode("utf-8")
         return e
 
+    def set(self,item,value) :
+        if item.find("user.")==-1 :
+            item=f"user.{item}"
+        if type(value) == type('') :
+            value=value.encode("utf-8")
+        xattr.set(self,item,value)
 
+    def items(self) :
+        for (k,v) in xattr.items(self) :
+            if k.find("user.")==0 :
+                k=k.split(".")[1]
+            yield (k,v)
 
 stattuple=namedtuple("stattuple",[b[0] for b in sorted([(a[3:].lower(),getattr(stat,a)) for a in dir(stat) if a.find("ST_")==0],key=lambda a: a[1])])
 
@@ -54,5 +67,18 @@ def test_metalist() :
 
 def test_template() :
     print(template.render(items=metalist("*")))
+
+def test_archieml() :
+    a=template.render(items=metalist("*"))
+    import pprint
+    pprint.pprint(archieml.loads(a))
+
+
+def run(path) :
+    print(template.render(items=metalist(path)))
+
+
+if __name__=='__main__' :
+    fire.Fire(run)
 
 
